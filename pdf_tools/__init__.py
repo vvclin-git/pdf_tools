66637b5da9bd15d7ab2262f2156aff6b21adb290
vvclin-git <vvclin@gmail.com>
2025-07-23 16:14:31 +0800

    Create __init__.py

---
+++ pdf_tools/__init__.py
@@ -0,0 +1,8 @@
+from .functions import (
+    clean_title,
+    encode_image_to_data_url,
+    extract_titles_from_images,
+    extract_outline_from_image,
+    images_to_pdf_with_nested_bookmarks,
+    merge_pdfs
+)
